f0d77a14f1095fa9363a455c118c2d2c0d61c87f
Prafful Varshney <79749572+praffulv-225@users.noreply.github.com>
2022-06-30 16:32:31 +0530

    Image-Colorization

--- infer.py
+++ infer.py
@@ -8,7 +8,7 @@ from utils import lab_to_rgb
 
 if __name__ == '__main__':
     model = MainModel()
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device('cpu')
     model.load_state_dict(
         torch.load(
             "final_model_weights.pt",
